f0e48070d8291760b68cefd23903f6b0721bf389
cammc <cam.mclagan@gmail.com>
2024-03-02 09:48:50 -0800

    test

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Trivia</h1>
+      <div className="card">
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
